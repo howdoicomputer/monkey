e9bd1a64ce39d0b1bfc7be146f8d0040a49f9bdc
Tyler Hampton <dr.frankinfurter@gmail.com>
2017-02-24 21:56:52 -0800

    : main file and beginning so writing down the token declarations

---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Printf("Hello %s! This is the Monkey programming language!\n",
+		"Tyler")
+
+	fmt.Printf("Feel to type in commands!\n")
+}
